336f9c0665cb9df1cdf7f429943e51547b22591d
Laricuquejo <larissa@cuquejo.org>
2022-01-31 17:25:18 +0100

    python3

---
+++ clase2.py
@@ -0,0 +1,18 @@
+persona1 = 90
+
+if type(persona1) == str:
+    print("isnt a number")
+    exit()
+
+elif persona1 > 18:
+    print("you can enter here")
+
+else:
+    print("you cant enter here")
+
+
+
+
+
+# I want you to creat a script that checks IF the person is yunger than 18 years old
+# check if its an integer else send an error message saying its not a number 
