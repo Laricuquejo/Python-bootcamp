57bfc9fdb4564ae113c2bd3dd66b9833a35185bc
Laricuquejo <larissa@cuquejo.org>
2022-02-01 13:31:01 +0100

    python4

---
+++ clase4.py
@@ -0,0 +1,16 @@
+grades_lookup = {"Kim Mathis": 7.5, "Laurence Hunt": 10, "Melody Ryan": 5, "Lester Patterson": 8}
+print(grades_lookup["Kim Mathis"])
+
+people = {"Leo", "Gaston", "Yahia", "Lari"}
+print(people)
+
+profesion = {"devops", "cocinero", "estudiante", "estudiante"}
+print(profesion)
+
+edad = {22, 22, 21, 19}
+print(edad)
+
+linea_lol = {"Mid", "Supp", "Mid", "Mid"}
+
+list = ['People', ["Leo", "Gaston", "Yahia", "Lari"]], ["Profesion", ["devops", "cocinero", "estudiante", "estudiante"]], ["edad", [22, 22, 21, 19]], ["linea-lol", ["Mid", "Supp", "Mid", "Mid"]]
+print(list)
