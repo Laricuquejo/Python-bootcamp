9b0a128f63c1f7ee7eaa965760531c9e354dec62
Laricuquejo <larissa@cuquejo.org>
2022-02-01 17:48:53 +0100

    python4

---
+++ clase5.py
@@ -0,0 +1,3 @@
+personas = [{"nombre": "Lari", "Edad": 19, "Profesion": "Programadora", "main-linea-lol": "Mid"}, {"nombre": "Leo", "Edad": 22, "Profesion": "Developer", "main-linea-lol": "Mid"}, {"nombre": "Gaston", "Edad": 22, "Profesion": "Cocinero", "main-linea-lol": "Supp"}, {"nombre": "Yahia", "Edad": 21, "Profesion": "Programador", "main-linea-lol": "Mid"}]
+print(personas[1][0])
+
