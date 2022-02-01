a17baf9a9a28bed77103072f44717c2090ebcc81
Laricuquejo <larissa@cuquejo.org>
2022-02-01 12:40:38 +0100

    python3

---
+++ clase3.py
@@ -0,0 +1,46 @@
+mylist = [1, 2, 3]
+
+print(len(mylist)) #to know how many things are in the list.
+
+print(mylist[0])
+
+print(mylist[1:]) #to go all the way to the end.
+
+anotherlist = ["four", "five"]
+
+print(mylist + anotherlist)
+
+newlist = mylist + anotherlist
+print(newlist)
+
+newlist[0] = "ONE"
+print(newlist)
+
+newlist.append("six") # to add something else to the list
+print(newlist)
+
+newlist.append("seven")
+print(newlist)
+
+newlist.pop() # to pop off an item
+
+print(newlist)
+
+newlist.pop()
+print(newlist)
+
+newlist.pop(0)
+print(newlist)
+
+newlist = ["Leo", "Gaston", "Lari"]
+agelist = ["22", "22", "19"]
+
+print(newlist + agelist)
+
+print(None)
+
+
+
+
+
+
