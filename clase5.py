47b9e9fe51099baa1bd406e9943244f882f2e03f
Laricuquejo <larissa@cuquejo.org>
2022-02-02 11:11:47 +0100

    python5

--- clase5.py
+++ clase5.py
@@ -1,3 +1,3 @@
-personas = [{"nombre": "Lari", "Edad": 19, "Profesion": "Programadora", "main-linea-lol": "Mid"}, {"nombre": "Leo", "Edad": 22, "Profesion": "Developer", "main-linea-lol": "Mid"}, {"nombre": "Gaston", "Edad": 22, "Profesion": "Cocinero", "main-linea-lol": "Supp"}, {"nombre": "Yahia", "Edad": 21, "Profesion": "Programador", "main-linea-lol": "Mid"}]
-print(personas[1][0])
+personas = [{"nombre": "Lari", "edad": 19, "profesion": "programadora", "main-linea-lol": "Mid"}, {"nombre": "Leo", "edad": 22, "profesion": "Developer", "main-linea-lol": "Mid"}, {"nombre": "Gaston", "edad": 22, "profesion": "Cocinero", "main-linea-lol": "Supp"}, {"nombre": "Yahia", "edad": 21, "profesion": "Programador", "main-linea-lol": "Mid"}]
+print(personas[0, 1, 2, 3]["nombre"])
 
---
+++ clase6.py
@@ -0,0 +1,16 @@
+mylist = ["manzana", "arroz", "platano"]
+
+for items in mylist:
+    print(items)
+
+mystring = "Good Morning"
+
+for letter in mystring:
+    print(letter)
+
+for _ in "Good Morning2":
+    print("jeje!")
+    print(len(mylist))
+
+for items in mylist:
+    print(items)
